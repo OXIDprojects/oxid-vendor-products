fe19835d9e1aa968a1766d3b70ad312f07632855
alexander <coding@aggrosoft.de>
2020-05-08 14:25:22 +0200

    add metadata version

--- copy_this/modules/agvendorproducts/metadata.php
+++ copy_this/modules/agvendorproducts/metadata.php
@@ -1,10 +1,13 @@
 <?php
+
+$sMetadataVersion = '1.1';
+
 $aModule = array(
     'id'           => 'agvendorproducts',
     'title'        => 'Vendor Products',
     'description'  => 'Adds getters and widgets for Oxid Vendor products',
     'thumbnail'    => '',
-    'version'      => '1.0.0',
+    'version'      => '1.0.1',
     'author'       => 'Aggrosoft GmbH',
     'extend'      => array(
         'oxvendor' => 'agvendorproducts/extensions/models/agvendorproducts_oxvendor'
